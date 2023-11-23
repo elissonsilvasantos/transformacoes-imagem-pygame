18ccd165932809c459bed7f49178d12307b2e3c5
Elisson Silva Santos <62728010+elissonsilvasantos@users.noreply.github.com>
2023-11-23 16:55:46 -0300

    projeto em construção

---
+++ programa edição de imagem.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.init()
+
+# configuração de tela
+largura_tela, altura_tela = 500, 500 # dimensões padrão
+tela = pygame.display.set_mode((largura_tela, altura_tela))
+pygame.display.set_caption('mover imagem')
+
+while True:
+    pass
+
+pygame.quit()
